b7087016f98e351db7911d01b347d22cc050b281
admin <admin@5D53>
2025-02-27 18:27:16 +0700

    New task added

---
+++ Programs/Practices/ReverseArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    internal class SolutionSlidingWindow
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 5, 1, 7, 8, 3, 1, 2, 12 };
+            ReverseArray(arr);
+            Console.WriteLine(string.Join(", ", arr));
+        }
+
+        static void ReverseArray(int[] arr)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (start < end)
+            {
+                // Меняем местами элементы
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                // Сдвигаем указатели
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ Programs/Practices/SlidingWindows/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    internal class SolutionSlidingWindow
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 2, 1, 7, 3, 10, 1, 2, 11, 15, 4, 3 };
+            Console.WriteLine(BadSolution(arr1,20));
+
+            int[] arr2 = new int[] { 2, 1, 7, 3, 10, 1, 2, 11, 15, 4, 3 };
+            Console.WriteLine(GoodSolution(arr2, 20));
+        }
+
+        static int BadSolution(int[] array, int targer)
+        {
+            int ans = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = i;
+                int sum = 0;
+                while(j<array.Length && sum + array[j] <= targer)
+                {
+                    sum += array[j++];
+                }
+                ans = Math.Max(ans, j - i);
+            }
+
+            return ans;
+        }
+
+        static int GoodSolution(int[] array, int target)
+        {
+            int ans = 0;
+            int left = 0, right = 0;
+            int current = 0;
+            while (right < array.Length)
+            {
+                current += array[right++];
+                while (current > target)
+                    current -= array[left++];
+
+                ans = Math.Max(ans, right - left);
+            }
+
+            return ans;
+        }
+    }
+}
