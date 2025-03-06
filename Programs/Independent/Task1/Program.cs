5dca0b8b7e586e19507a15087d3c18a6a84e373d
admin <admin@5D53>
2025-03-06 19:39:22 +0700

    New tasks have been added

---
+++ Programs/Independent/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Написать метод класса, который на вход получает отсортированный по
+// неубыванию массив целых чисел и целое число target. Метод возвращения
+// массива из двух элементов находящихся в этом массиве, сумма которых равна targer.
+// Если таких элементов нет, то возв
+
+// Пример 1: 
+// массив = (-10, -2, 0, 3, 6, 12, 20)
+// target = 1
+// вернет: [-2,3]
+
+// Пример 2: 
+// массив = (-10, -2, 0, 3, 6, 12, 20)
+// target = -1
+// вернет: []
+
+using System;
+
+class Program
+{
+    static int[] FindTwoSum(int[] nums, int target)
+    {
+
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right)
+        {
+            int sum = nums[left] + nums[right];
+
+            if (sum == target) return new int[] { nums[left], nums[right] };
+            else if (sum < target) left++;
+            else right--;
+        }
+
+        return new int[] { };
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array1 = { -10, -2, 0, 3, 6, 12, 20 };
+        int target1 = 1;
+        int[] result1 = FindTwoSum(array1, target1);
+        Console.WriteLine("Пример 1: [" + string.Join(", ", result1) + "]");
+    }
+}
---
+++ Programs/Independent/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать метод класса, который на вход получает массив положительных целых
+// чисел и положительное целое число targer. Вернуть минимальную длину подмассива,
+// сумма которых больше либо равна targer. Если такого подмассива нет, то вернуть 0.
+
+// Пример 1: targer = 7[2, 3, 1, 2, 4, 3] - выведет - 2
+// Пример 2: targer = 4[1, 4, 4] - выведет - 1
+// Пример 3: targer = 11 х[1, 4, 4] - выведет - 0
+
+using System;
+
+class Program
+{
+    public static int MinSubArrayLen(int target, int[] nums)
+    {
+        int minLength = int.MaxValue;
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            sum += nums[right];
+
+            while (sum >= target)
+            {
+                minLength = Math.Min(minLength, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLength == int.MaxValue ? 0 : minLength;
+    }
+
+    static void Main(string[] args)
+    {
+        int target = 7;
+        int[] nums = { 2, 3, 1, 2, 4, 3 };
+        Console.WriteLine(MinSubArrayLen(target, nums));
+    }
+}
