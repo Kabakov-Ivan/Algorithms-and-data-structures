d4b47037208eaff3047ae47391eb82d9138fac4b
admin <admin@5D53>
2025-04-24 14:34:40 +0700

    New tasks have been added

---
+++ Programs/Practices/PointVectorDemo/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Test
+{
+    public struct Point
+    {
+        public double x;
+        public double y;
+
+        public Point(double x, double y)
+        {
+            // this хранит ссылку на объект
+            this.x = x;
+            this.y = y;
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"({x};{y})";
+        //}
+
+        public override string ToString() => $"({x};{y})";
+
+        public int GetQuarter()
+        {
+            if (x > 0 && y > 0) return 1;
+            if (x > 0 && y > 0) return 2;
+            if (x > 0 && y > 0) return 3;
+            if (x > 0 && y > 0) return 4;
+
+            return 0;
+        }
+
+        public bool IsSameQuarter(Point point) => GetQuarter() == point.GetQuarter();
+    }
+}
---
+++ Programs/Practices/PointVectorDemo/Vector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public struct Vector
+    {
+        public Point beggining;
+        public Point end;
+
+        public Vector(Point beggining, Point end)
+        {
+            this.beggining = beggining;
+            this.end = end;
+        }
+
+        public Vector(double x1, double y1, double x2, double y2)
+        {
+            beggining = new Point(x1, y1);
+            end = new Point(x2, y2);
+        }
+
+        private double lenhth;
+        public double Lenght
+        {
+            get
+            {
+                return lenhth;
+            }
+            private set
+            {
+                lenhth = Math.Sqrt(Math.Pow(end.x - beggining.x, 2)
+                    + Math.Pow(end.y - beggining.y, 2));
+
+            }
+        }
+    }
+}
