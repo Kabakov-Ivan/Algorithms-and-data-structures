e5ff65a2112240dbca2c14c836106df173ac85dd
admin <admin@5D53>
2025-04-24 18:13:14 +0700

    New tasks have been added

---
+++ Programs/Practices/PersonalList/ListNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalList
+{
+    // Объект этого класса ListNode прелставялет узел списка хранящий
+    // целочисленную инфоомацию и ссылку на следующий узел
+    class ListNode
+    {
+        public int Data;
+        public ListNode Next;
+
+        public ListNode(int data, ListNode next = null) 
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
---
+++ Programs/Practices/PersonalList/Program.cs
@@ -0,0 +1,60 @@
+﻿using PersonalList;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+
+namespace qweqwe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode node = new ListNode(5);
+            node.Next = new ListNode(4);
+            node.Next.Next = new ListNode(12);
+            ListNode tmp = node.Next;
+            node.Next = new ListNode(1);
+            node.Next = tmp;
+            Print(node);
+
+            Insert(ref node, 23, 0);
+            Print(node);
+
+            Insert(ref node, 100, 2);
+            Insert(ref node, 34, 100);
+            Print(node);
+
+        }
+
+        static void Print(ListNode head)
+        {
+            if (head is null)
+                return;
+            int i = 0;
+
+            while (head is not null)
+            {
+                Console.WriteLine($"Elem {++i}: {head.Data}\t");
+                head = head.Next;
+            }
+            Console.WriteLine();
+        }
+
+        static void Insert(ref ListNode head, int value, int position)
+        {
+            if (head is null || position < 0) return;
+            if (position == 0)
+            {
+                head = new ListNode(value, head);
+                return;
+            }
+
+            ListNode tmp = head;
+            for (int i = 0; i < position - 1 && tmp.Next is not null; i++, head = tmp.Next) ;
+            
+            ListNode newNode = new ListNode(value, tmp.Next);
+            tmp.Next = newNode;
+        }
+    }
+}
